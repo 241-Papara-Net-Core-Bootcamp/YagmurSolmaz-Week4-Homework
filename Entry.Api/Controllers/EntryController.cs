dd2c4da3e5facf48d18819140315ca910f6bc006
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-11-20 01:55:32 +0300

    seconcommit

---
+++ Entry.Api/Controllers/EntryController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Entry.Data.Dtos;
+using Entry.Service.Abstracts;
+using System.Threading.Tasks;
+using System;
+
+namespace Entry.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntryController : ControllerBase
+    {
+        private readonly IEntryService _entryService;
+        public EntryController(IEntryService entryService)
+        {
+            _entryService = entryService;
+        }
+
+        [HttpGet("GetEntries")]
+        public async Task<IActionResult> GetEntries()
+        {
+            var entryList = await _entryService.GetAllAsync();
+            return Ok(entryList);
+
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetEntryById(string name)
+        {
+            var entry = await _entryService.GetAsync(name);
+            return Ok(entry);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEntry(EntryDto entryDto)
+        {
+
+            await _entryService.AddAsync(entryDto);
+            return Ok();
+
+        }
+
+        [HttpPut("{name}")]
+        public async Task<IActionResult> UpdateEProduct(EntryDto entryDto, string name)
+        {
+
+            await _entryService.Update(entryDto, name);
+            return Ok();
+        }
+
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteEntry(string name)
+        {
+            await _entryService.Delete(name);
+            return Ok();
+        }
+    }
+}
