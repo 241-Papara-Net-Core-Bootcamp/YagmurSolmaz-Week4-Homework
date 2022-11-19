894b71d9c8b3b6b98854906252fa7fc50813d05e
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-11-20 02:10:33 +0300

    thirdcommit

---
+++ Entry.Domain/Entities/EntryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entry.Domain.Entities
+{
+    public class EntryEntity 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public System.Guid Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Entry { get; set; }
+
+    }
+}
